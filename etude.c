2e4fcf30af7b4409212e29ad172d806cb54796d6
Divine <irasubizadivine009@gmail.com>
2025-01-20 12:08:27 +0200

    Add files via upload

---
+++ etude.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+/*int main(){
+	int a;
+	a=1;
+	for(a=1;a<=10;a++){
+		printf("%d\n",a);
+	}
+	return 0;
+}*/
+
+/*int main(){
+	int a,sum;
+	a=1;
+	sum=0;
+	for(a=1;a<=50;a++){
+		if(a%2==0){
+			sum=sum+a;	
+		}	
+	}
+	printf("The sum is: %d\n",sum);
+	return 0;
+}*/
+
+/*int main(){
+	int arr[5]={2,4,6,8,10};
+	
+	for(int i=0;i<5;i++){
+	printf("elements of the array are: %d\n", arr[i]);		
+}
+printf("Element 1 of the array are: %d\n",arr[0]);
+	return 0;
+}*/
+
+int main(){
+	int array[6]={1,2,3,4,5,6};
+	int max,min,i=0;
+	for(i=0;i<6;i++){
+		if 
+	}
+	
+	return 0;
+}
+
+/*int main(){
+	int i,n,arr[i];
+	printf("Enter any array you want:");
+	scanf("%d%d",&n,&i);
+	n=arr[i];
+	printf("you entered: %d\n",arr[i]);
+	
+	return 0;
+}*/
+
+
+//int main(){
+//	int myArr[7];
+//	int i=0;
+//	while(i<7){
+//		printf("%d\n",myArr[i]);
+//		i++;
+//	}
+//	
+//	return 0;
+//}
+
+
+
+
+/*int main(){
+	int myArray={4,5,6,-8,-2,0,9,-5};
+	int i;
+	int sum=0;
+	for(i=0;i<myArray.length;i++){
+		sum+=myArray[i];
+	}
+	printf("the sum of elnts is: %d\n",sum);
+	return 0;
+}*/
